57820a86dd37c092e79dd41d52fab3106d327261
Lien! <liensilviosanchez@gmail.com>
2022-01-18 14:43:35 -0300

    correciones

--- app.js
+++ app.js
@@ -1,122 +1,35 @@
-//ejercicio
-let boton=document.getElementById('siQuieroBoton');
-boton.addEventListener('click', function(){
-  //muestro foto de gato
-  document.getElementById("gatito").style.display="block";
-  //creo un nuevo boton
-  let nuevoBoton=document.createElement("button");
-  //edito el boton
-  nuevoBoton.innerHTML="Quiero mas fotos!";
-  //agregando boton 
-  document.body.appendChild(nuevoBoton);
-  //funcion nuevoBoton
-  nuevoBoton.addEventListener('click',function(){
-    //muestro foto de gato1
-    document.getElementById("gatito1").style.display="block";
-    //creo un nuevo boton1
-    let nuevoBoton1=document.createElement("button");
-    //elimino boton
-    nuevoBoton.parentNode.removeChild(nuevoBoton);
-    //edito el boton
-    nuevoBoton1.innerHTML="Quiero mas fotos!";
-    //agregando boton 
-    document.body.appendChild(nuevoBoton1);
-      //funcion nuevoBoton1
-      nuevoBoton1.addEventListener('click',function(){
-      //muestro foto de gato2
-      document.getElementById("gatito2").style.display="block";
-      //creo un nuevo boton2
-      let nuevoBoton2=document.createElement("button");
-      //elimino boton
-      nuevoBoton1.parentNode.removeChild(nuevoBoton1);
-      //edito el boton
-      nuevoBoton2.innerHTML="Quiero mas fotos!";
-      //agregando boton 
-      document.body.appendChild(nuevoBoton2);
-        //funcion nuevoBoton2
-        nuevoBoton2.addEventListener('click',function(){
-        //muestro foto de gato3
-        document.getElementById("gatito3").style.display="block";
-        //creo un nuevo boton3
-        let nuevoBoton3=document.createElement("button");
-        //elimino boton
-        nuevoBoton2.parentNode.removeChild(nuevoBoton2);
-        //edito el boton
-        nuevoBoton3.innerHTML="Quiero mas fotos!";
-        //agregando boton 
-        document.body.appendChild(nuevoBoton3);
-        //funcion nuevoBoton3
-        nuevoBoton3.addEventListener('click',function(){
-          alert("cuantas fotos queres???");
-        })
-      })
-    })
-  })  
-})
 
-let noBoton=document.getElementById('noQuieroBoton');
-noBoton.addEventListener('click', function(){
-  //creamos elemento parrafo
-  let parrafo = document.createElement("p");
-  //editamos el texto
-  parrafo.innerHTML="<h1>Usted eligio no ver fotos de gatos, eso significa que esta EQUIVOCADO!</h1>";
-  //agregamos el elemento
-  document.body.appendChild(parrafo);
-  //muestro foto de gato
-  document.getElementById("gatito").style.display="block";
-  //creo un nuevo boton
-  let nuevoBoton=document.createElement("button");
-  //edito el boton
-  nuevoBoton.innerHTML=" NO quiero mas fotos!";
-  //agregando boton 
-  document.body.appendChild(nuevoBoton);
-  //funcion nuevoBoton
-  nuevoBoton.addEventListener('click',function(){
-    //muestro foto de meme1
-    document.getElementById("meme1").style.display="block";
-    //creo un nuevo boton1
-    let nuevoBoton1=document.createElement("button");
-    //elimino boton
-    nuevoBoton.parentNode.removeChild(nuevoBoton);
-    //elimino parrafo
-    parrafo.parentNode.removeChild(parrafo);
-    //edito el boton
-    nuevoBoton1.innerHTML="Aprendi, quiero otra foto!";
-    //agregando boton 
-    document.body.appendChild(nuevoBoton1);
-      //funcion nuevoBoton1
-      nuevoBoton1.addEventListener('click',function(){
-      //muestro foto de gato2
-      document.getElementById("gatito2").style.display="block";
-      //creo un nuevo boton2
-      let nuevoBoton2=document.createElement("button");
-      //elimino boton
-      nuevoBoton1.parentNode.removeChild(nuevoBoton1);
-      //elimino meme
-      meme1.parentNode.removeChild(meme1);
-      //edito el boton
-      nuevoBoton2.innerHTML="Quiero mas fotos!";
-      //agregando boton 
-      document.body.appendChild(nuevoBoton2);
-        //funcion nuevoBoton2
-        nuevoBoton2.addEventListener('click',function(){
-        //muestro foto de gato3
-        document.getElementById("gatito3").style.display="block";
-        //creo un nuevo boton3
-        let nuevoBoton3=document.createElement("button");
-        //elimino boton
-        nuevoBoton2.parentNode.removeChild(nuevoBoton2);
-        //edito el boton
-        nuevoBoton3.innerHTML="Quiero mas fotos!";
-        //agregando boton 
-        document.body.appendChild(nuevoBoton3);
-        //funcion nuevoBoton3
-        nuevoBoton3.addEventListener('click',function(){
-          alert("cuantas fotos queres???");
-        })
-      })
-    })
-  })  
+//ejercicio mouseOver
+let imagen = document.getElementById('imagen');
+imagen.addEventListener("mouseover", function (){
+
+ 
+  //creo un boton para promocion
+  let promocionBoton = document.createElement("button");
+  //edito el contenido del boton
+  promocionBoton.innerHTML = "quiero saber mas de la promocion"; 
+  //agrego el boton
+  document.body.appendChild(promocionBoton);
+
+  //voy a crear un form para que rellene un dato
+  promocionBoton.addEventListener("click", function(){
+    let datos = document.createElement('form');
+    datos.innerHTML='<input placeholder="nombre" type="text"></input><button type="sumbit">enviar </button>';
+    document.body.appendChild(datos);
+  
+  //formulario
+  datos.onsubmit = function(event){
+    //evito q se recargue la pagina
+    event.preventDefault();
+    const input = event.target.children;
+    //creo link para comunicarse por whatsapp
+    let link = document.createElement('a');
+    link.innerHTML = '<a href="https://api.whatsapp.com/send?phone=2612167506&text=hola%20soy">ir a whatsapp</a>'
+    document.body.appendChild(link);
+  }
+  })
+    
+
 })
 
 
